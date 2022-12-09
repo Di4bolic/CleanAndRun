72ad11a7a0ddc6493f11b66786ad9fe2edcbf5b7
Ethan Mugnaïni de Ricci <e.mugnainidericci@rns.ecolescreatives.com>
2022-12-09 15:58:51 +0100

    AvancéesGarbages

--- Assets/Project/Scripts/Play/SpawnGarbages.cs
+++ Assets/Project/Scripts/Play/SpawnGarbages.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SpawnGarbages : MonoBehaviour
 {
+    public GameObject garbagePrefab;
+    public GameObject spawnPoint;
+
+    float chrono;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        chrono = 5f + Random.Range(0, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (chrono > 0)
+        {
+            chrono -= Time.deltaTime;
+        }
+        else
+        {
+            chrono = 5f + Random.Range(0, 5);
+            SpawnGarbage();
+        }
+    }
+
+    void SpawnGarbage()
+    {
+        Instantiate(garbagePrefab, spawnPoint.transform.position, Quaternion.identity);
     }
 }
