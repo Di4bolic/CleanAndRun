ada5815b190db0cc2061067d92874034ecfcba55
Ethan Mugnaïni de Ricci <e.mugnainidericci@rns.ecolescreatives.com>
2022-12-13 11:57:19 +0100

    AvancéesPause

--- Assets/Project/Scripts/Play/Pause.cs
+++ Assets/Project/Scripts/Play/Pause.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Pause : MonoBehaviour
 {
+    public AudioSource aS;
+    public TextMeshProUGUI pauseButton;
+    public bool isPaused = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +23,21 @@ public class Pause : MonoBehaviour
 
     public void PauseGame()
     {
-        Time.timeScale = 0;
-    }
+        if (isPaused)
+        {
+            pauseButton.text = "|>";
+            Time.timeScale = 1;
+            aS.Play();
+            isPaused = false;
+        }
+        else
+        {
+            pauseButton.text = "| |";
+            Time.timeScale = 0;
+            aS.Pause();
+            isPaused = true;
+        }
+        
 
-    public void ResumeGame()
-    {
-        Time.timeScale = 1;
     }
 }
