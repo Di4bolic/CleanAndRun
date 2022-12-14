24c5989cda4dbe1b2623f658d9c3be7f655c5cb2
Ethan Mugnaïni de Ricci <e.mugnainidericci@rns.ecolescreatives.com>
2022-12-14 10:16:11 +0100

    Avancées

--- Assets/Project/Scripts/Play/MoveProjectil.cs
+++ Assets/Project/Scripts/Play/MoveProjectil.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class MoveProjectil : MonoBehaviour
 {
-    float projectilSpeed = 20f;
+    float projectilSpeed = 15f;
+    float rotationSpeed = 300f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,7 @@ public class MoveProjectil : MonoBehaviour
     {
 
         transform.position += Vector3.right * projectilSpeed * Time.deltaTime;
+        transform.eulerAngles += new Vector3(0, 0, 1) * Time.deltaTime * -rotationSpeed;
         // Destruction du GameObject
         if (transform.position.x >= 15)
         {
