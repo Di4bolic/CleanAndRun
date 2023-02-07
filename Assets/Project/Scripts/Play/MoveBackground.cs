cf3b1ad1f8029e235482868a65b9298a35a3a20e
Ethan Mugnaïni de Ricci <e.mugnainidericci@rns.ecolescreatives.com>
2023-02-07 09:58:11 +0100

    Avancées

---
+++ Assets/Project/Scripts/Play/MoveBackground.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    public ObstacleManager oM;
+    private float lengthOfMusic;
+
+    [SerializeField]
+    private Transform finMove;
+
+    private Vector3 oldPos;
+
+    private float pourcentage = 0f;
+
+    private float speed = 0.001f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldPos = transform.position;
+        lengthOfMusic = oM.musicManager.lengthMusic;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(oldPos, finMove.position, pourcentage);
+        pourcentage += speed;
+    }
+}
