0f10b30eb400cdbadc67c36ddb72262492e7a39c
Ethan Mugnaïni de Ricci <e.mugnainidericci@rns.ecolescreatives.com>
2023-02-08 11:54:32 +0100

    Avancées

--- Assets/Project/Scripts/Play/RandomGarbage.cs
+++ Assets/Project/Scripts/Play/RandomGarbage.cs
@@ -10,6 +10,9 @@ public class RandomGarbage : MonoBehaviour
     [SerializeField]
     private List<Sprite> listSprites;
 
+    [SerializeField]
+    private GameObject feedback;
+
     // Start is called before the first frame update
     void Start()
     {
