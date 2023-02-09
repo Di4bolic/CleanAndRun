a135623276ffe0e8a31c3200d447fdd80d92cd38
ballonde <93644915+ballonde@users.noreply.github.com>
2023-02-09 15:58:05 +0100

    AffinageTuto

--- Assets/Project/Scripts/Tuto/ManagerTuto.cs
+++ Assets/Project/Scripts/Tuto/ManagerTuto.cs
@@ -16,6 +16,7 @@ public class ManagerTuto : MonoBehaviour
     public TextMeshProUGUI textDoubleJump;
     public TextMeshProUGUI textGarbage;
     public TextMeshProUGUI textShoot;
+    public TextMeshProUGUI textGood;
 
     public Boss boss;
     public Player player;
@@ -29,19 +30,36 @@ public class ManagerTuto : MonoBehaviour
 
     void Update()
     {
-        if (ob1.gameObject.transform.position.x <= -10)
+        if (ob1.gameObject.transform.position.x <= -10 && ob1.gameObject.transform.position.x >= -12)
         {
             textJump.gameObject.SetActive(false);
             textDoubleJump.gameObject.SetActive(true);
         }
 
-        if (ob2.gameObject.transform.position.x <= -10)
+        if (player.stun==true && ob1.gameObject.transform.position.x <= 5 && ob1.gameObject.transform.position.x >= -12)
+        {
+            ob1.transform.position = new Vector3(25, -7, 0);
+            ob2.transform.position = new Vector3(50, -3, 0);
+            ob3.transform.position = new Vector3(80, -1, 0);
+            garbage1.transform.position = new Vector3(80, -0, 0);
+            garbage2.transform.position = new Vector3(87, -0, 0);
+        }
+
+        if (ob2.gameObject.transform.position.x <= -10 && ob2.gameObject.transform.position.x >= -12)
         {
             textDoubleJump.gameObject.SetActive(false);
             textGarbage.gameObject.SetActive(true);
         }
 
-        if (player.munitionsScore<=0 && ob3.gameObject.transform.position.x <= -15 && endTuto == false)
+        if (player.stun == true && ob2.gameObject.transform.position.x <= 5)
+        {
+            ob2.transform.position = new Vector3(25, -3, 0);
+            ob3.transform.position = new Vector3(65, -1, 0);
+            garbage1.transform.position = new Vector3(65, -0, 0);
+            garbage2.transform.position = new Vector3(67, -0, 0);
+        }
+
+        if (player.munitionsScore<=0 && ob3.gameObject.transform.position.x >= -20 && ob3.gameObject.transform.position.x <= -10 && endTuto == false)
         {
             ob3.transform.position = new Vector3(25, -1, 0);
             garbage1.transform.position = new Vector3(22, -0, 0);
@@ -53,10 +71,12 @@ public class ManagerTuto : MonoBehaviour
             textGarbage.gameObject.SetActive(false);
             textShoot.gameObject.SetActive(true);
             boss.SpawnBoss();
+            endTuto = true;
         }
 
         if (boss.lifeBoss <= 0){
-            endTuto = true;
+            textShoot.gameObject.SetActive(false);
+            textGood.gameObject.SetActive(true);
         }
     }
 }
